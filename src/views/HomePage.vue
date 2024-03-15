df6a15a57ffb59531834fbcd73e8dd542b4f98e1
Bui-Thanh-Liem <buithanhliem5073@gmail.com>
2024-03-15 17:37:43 +0700

    edit-homePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,78 @@
 <template>
   <section>
     <section>
-      <img class="w-100" src="../../src/assets/imgs/123.jpg" />
+      <!-- Carosel -->
+      <div
+        id="carouselExampleIndicators"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="../../src/assets/imgs/home1.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="../../src/assets/imgs/home2.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="../../src/assets/imgs/home3.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <!--  -->
       <div class="row">
         <div class="col-md-4">
           <div class="card shadow mt-4" style="background-color: #96EFFF">
@@ -89,77 +160,6 @@
             </div>
           </div>
         </div>
-      </div>
-      <br />
-
-      <div
-        id="carouselExampleIndicators"
-        class="carousel slide"
-        data-ride="carousel"
-      >
-        <ol class="carousel-indicators">
-          <li
-            data-target="#carouselExampleIndicators"
-            data-slide-to="0"
-            class="active"
-          ></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              src="../../src/assets/imgs/home1.jpg"
-              class="d-block w-100"
-              alt="..."
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="../../src/assets/imgs/home2.jpg"
-              class="d-block w-100"
-              alt="..."
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="../../src/assets/imgs/home3.jpg"
-              class="d-block w-100"
-              alt="..."
-            />
-          </div>
-        </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-      <br />
-
-      <h3 class="text-center" style="color: #444444">
-        <strong>ĐỐI TÁC HOẠT ĐỘNG</strong>
-      </h3>
-      <div class="owl-carousel owl-theme">
-        <div class="item"><img src="../../src/assets/imgs/client1.png" /></div>
-        <div class="item"><img src="../../src/assets/imgs/client2.png" /></div>
-        <div class="item"><img src="../../src/assets/imgs/client3.png" /></div>
-        <div class="item"><img src="../../src/assets/imgs/client4.png" /></div>
-        <div class="item"><img src="../../src/assets/imgs/client5.png" /></div>
-        <div class="item"><img src="../../src/assets/imgs/client6.png" /></div>
       </div>
     </section>
   </section>
